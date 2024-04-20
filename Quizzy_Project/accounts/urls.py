e1cfa7335abe1862f9f1999cf232e5d5d3186335
KirsteenJ <155801140+KirsteenJ@users.noreply.github.com>
2024-04-20 21:59:49 +0800

    Delete Quizzy_Project directory

--- Quizzy_Project/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-# acounts/urls.py
-from django.urls import path
-from . import views # the “.” means current directory
-urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-]
